bbb003360bd16cb5af70a9be14264ab94cc23b9a
Faishal <faishal@scrambleapps.com>
2022-09-13 13:07:19 +0530

    Initial Commit

---
+++ OTP Verification/OTP_VerificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  OTP_VerificationApp.swift
+//  OTP Verification
+//
+//  Created by faishal on 13/09/22.
+//
+
+import SwiftUI
+
+@main
+struct OTP_VerificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
